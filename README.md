058ca8a8796f2631f325c8c5a26ed91ad4a9497b
Safiq F <syafiqfaray2@gmail.com>
2021-03-21 12:09:17 +0700

    penambahan gui

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # tubesstima_amogus
 tubes stima
 
+Source code berada di directory /testing/testing
 ### REFERENSI
 * https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 * https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
---
+++ testing/testing/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace testing
+{
+    public partial class Form1 : Form
+    {
+        string[] input;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //browse button
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //search button
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //upload button
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //select account
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //explore friends with
+        }
+    }
+}
