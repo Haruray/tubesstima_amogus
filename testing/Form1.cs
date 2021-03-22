91653ca2b5f213e11dded306cb22844a3a2e6576
bryanrinaldoo <bryan.rinaldo24@gmail.com>
2021-03-22 14:33:38 +0700

    update form1

--- testing/Form1.cs
+++ testing/Form1.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
-using System.Drawing;
 using System.Drawing.Imaging;
 using System.Web;
 
@@ -22,10 +21,15 @@ namespace tubesstima
     public partial class Form1 : Form
     {
         string[] input;
+        string[] temp1;
+        string[] temp2;
         string method;
         int size;
         Graph g;
+<<<<<<< HEAD
+=======
 
+>>>>>>> fa27a9b46d05d01b8bd7d73c1c7729e794d98e67
         public Form1()
         {
             InitializeComponent();
@@ -50,6 +54,10 @@ namespace tubesstima
         private void button3_Click(object sender, EventArgs e)
         {
             //search button
+<<<<<<< HEAD
+            List<string> test = g.SearchPath("A", "C", method);
+            
+=======
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -84,11 +92,14 @@ namespace tubesstima
             //create a viewer object 
             //Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
             //create a graph object 
+>>>>>>> fa27a9b46d05d01b8bd7d73c1c7729e794d98e67
             Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
-            //create the graph content 
-            graph.AddEdge("A", "B");
-            graph.AddEdge("B", "C");
-            graph.AddEdge("A", "C");
+            
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                graph.AddEdge(temp1[i], temp2[i]);
+            }
             for (int i = 0; i < test.Count; i++)
             {
                 graph.FindNode(test[i]).Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
@@ -96,15 +107,49 @@ namespace tubesstima
 
             Microsoft.Msagl.GraphViewerGdi.GraphRenderer renderer = new Microsoft.Msagl.GraphViewerGdi.GraphRenderer(graph);
             renderer.CalculateLayout();
+<<<<<<< HEAD
+
+=======
             //bind the graph to the viewer 
             //associate the viewer with the form 
             //form.FormBorderStyle = 
             //Microsoft.Msagl.Drawing.SvgGraphWriter.Write(graph, @"C:\Users\SAFIQ FARAY\source\repos\testing\testing\bin\Debug\graph.svg");
+>>>>>>> fa27a9b46d05d01b8bd7d73c1c7729e794d98e67
             int width = 150;
             Bitmap bitmap = new Bitmap(width, (int)(graph.Height * (width / graph.Width)), System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
             renderer.Render(bitmap);
             bitmap.Save("graph.png");
             pictureBox1.Image = bitmap;
+<<<<<<< HEAD
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //upload button
+            File.Copy(textBox1.Text, Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileName(textBox1.Text)), true);
+            input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileName(textBox1.Text)));
+            string alltext = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileName(textBox1.Text)));
+            int graphSize = alltext.Distinct().Count();
+            textBox1.Text = input[0];
+            size = Int32.Parse(input[0]);
+            g = new Graph(graphSize);
+            temp1 = new string[size];
+            temp2 = new string[size];
+            int j = 0;
+            for (int i = 1; i < size; i++)
+            {
+                string[] row = input[i].Split(' ');
+                temp1[j] = row[0];
+                temp2[j] = row[1];
+                j++;
+            }
+            textBox1.Text = "Berhasil!";
+            for (int i = 0; i < size - 1; i++)
+            {
+                g.NewEdge(temp1[i], temp2[i]);
+            }
+=======
+>>>>>>> fa27a9b46d05d01b8bd7d73c1c7729e794d98e67
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
